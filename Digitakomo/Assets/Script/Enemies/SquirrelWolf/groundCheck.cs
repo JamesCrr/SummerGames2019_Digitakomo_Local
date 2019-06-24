d691a5b4f2e40facd60ace06d16eb6eaac4d7ca7
Wei <g0546699m@gmail.com>
2019-06-24 14:13:12 +0300

    changed way wolf detecting ground

---
+++ Digitakomo/Assets/Script/Enemies/SquirrelWolf/groundCheck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class groundCheck : MonoBehaviour
+{
+    public SquirrelWolf attachedObj = null;
+    [System.NonSerialized]
+    public bool startCheck = false;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (startCheck == false)
+            return;
+        if (collision.gameObject.layer != LayerMask.GetMask("Ground"))
+            return;
+
+        attachedObj.isGrounded = true;
+        startCheck = false;
+    }
+}
