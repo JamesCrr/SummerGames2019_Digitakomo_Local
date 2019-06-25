c12c703ba0c14d8d2d4ce443dcfc9d082328ecb0
ziveso <thitiwat.thongbor@gmail.com>
2019-06-25 11:29:36 +0300

    add item layer

--- Digitakomo/Assets/Script/Items/GoldenFeather.cs
+++ Digitakomo/Assets/Script/Items/GoldenFeather.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GoldenFeather : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("colliderenter");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("trigger");
     }
 }
