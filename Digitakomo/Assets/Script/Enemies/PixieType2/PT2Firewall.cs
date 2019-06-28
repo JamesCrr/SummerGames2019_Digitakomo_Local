d7b2de9e403e83a7eb4dc9944c1969feb196084c
ziveso <thitiwat.thongbor@gmail.com>
2019-06-28 16:15:28 +0300

    add firewall damage

---
+++ Digitakomo/Assets/Script/Enemies/PixieType2/PT2Firewall.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PT2Firewall : MonoBehaviour
+{
+    [SerializeField]
+    float MinDamage = 15f;
+    [SerializeField]
+    float MaxDamage = 20f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("PlayerProj"))
+        {
+            // if hit by fire projectile
+            Weapon weapon = collision.gameObject.GetComponent<Weapon>();
+            if (weapon.at == AttackType.ICE)
+            {
+                gameObject.SetActive(false);
+            }
+        }
+        // hit player
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collision.gameObject.GetComponent<IDamagable>().TakeDamage(Random.Range(MinDamage, MaxDamage));
+            gameObject.SetActive(false);
+        }
+    }
+}
