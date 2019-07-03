6aef870a70d567c8af9eb4d958edaf9c2ac0622a
Wei <g0546699m@gmail.com>
2019-07-03 13:41:05 +0300

    fixed levitating bug

---
+++ Digitakomo/Assets/Script/Enemies/ApesWolf/AWGroundCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AWGroundCheck : MonoBehaviour
+{
+    public AWScript attachedObj = null;
+    [System.NonSerialized]
+    public Platforms platformStandingOn = null;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
+            return;
+
+        // attach platform if have
+        if (collision.GetComponent<Platforms>())
+            platformStandingOn = collision.GetComponent<Platforms>();
+        // We landed on the ground
+        attachedObj.LandedGround();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Ground"))
+            return;
+        // Left the ground
+        attachedObj.LeftGround();
+    }
+}
