5b12d1c784a79661b105e594c44105514e827127
ziveso <thitiwat.thongbor@gmail.com>
2019-07-09 14:55:32 +0300

    Add Game manager for dev

---
+++ Digitakomo/Assets/Script/Games/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int PlayerCount = 2;
+    public int score = 0;
+
+    [Header("This should be initialize already on MainMenu")]
+    [Header("In case you're in development scene")]
+    public GameObject ManagersPrefab;
+    public GameObject SoundsPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize the player somehow
+        if (InputManager.Instance == null)
+        {
+            Instantiate(ManagersPrefab);
+        }
+    }
+}
