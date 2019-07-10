f28b6e4ed252aee23b244a69ac34b85fcf8fead9
Wei <g0546699m@gmail.com>
2019-07-10 13:59:30 +0300

    added textmeshpro and testing scoreboard

---
+++ Digitakomo/Assets/Script/ScoreBoard/ScoreFileManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;   // To allow for access to binary files
+using System.IO;
+using UnityEngine;
+
+public class ScoreFileManager : MonoBehaviour
+{
+    // To store filePath for single Player
+    static string singlePlayerFilePath;
+    static List<ScoreSaveData> listOfScores = new List<ScoreSaveData>();
+
+    private void Awake()
+    {
+        singlePlayerFilePath = Path.Combine(Application.persistentDataPath, "singleScoreData.sc");
+    }
+
+    
+    // Saving Single Player Score
+    public void Single_SaveNewScore(string newName, int newScore)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(singlePlayerFilePath, FileMode.Create);
+        // If File already exists, then load existing data first
+        if (!File.Exists(singlePlayerFilePath))
+            Single_LoadScores();
+        
+        // Convert raw data and add into list
+        ScoreSaveData newData = new ScoreSaveData(newName, newScore);
+        listOfScores.Add(newData);
+        // Serialise into Binary File
+        bf.Serialize(stream, listOfScores);
+
+        // Close the stream
+        stream.Close();
+        return;
+    }
+
+
+    // Loading Single Player Scores
+    public List<ScoreSaveData> Single_LoadScores()
+    {
+        // If File does not exist, return false
+        if (!File.Exists(singlePlayerFilePath))
+            return null;
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(singlePlayerFilePath, FileMode.Open);
+        // Deserialize the Data and assign to list
+        listOfScores = bf.Deserialize(stream) as List<ScoreSaveData>;
+        // Close the stream
+        stream.Close();
+
+        return listOfScores;
+    }
+}
+
+
+
+
+[System.Serializable]   // To allow unity to read and write from this class
+public class ScoreSaveData
+{
+    public string plyrName;
+    public int plyrScore;
+
+    public ScoreSaveData(string name, int score)
+    {
+        plyrName = name;
+        plyrScore = score;
+    }
+}
---
+++ Digitakomo/Assets/Script/ScoreBoard/ScoreUIManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreUIManager : MonoBehaviour
+{
+    // Public Instance
+    public static ScoreUIManager Instance = null;
+
+    [SerializeField]
+    ScoreFileManager fileManager = null;
+
+    // The Score and Name Panel 
+    [SerializeField]
+    GameObject scorePanel = null;
+    [SerializeField]
+    GameObject namePanel = null;
+    // The Text Prefab to use
+    [SerializeField]
+    GameObject text;
+
+    // Awake
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    // Start
+    private void Start()
+    {
+        fileManager.Single_SaveNewScore("wow,", 132);
+        fileManager.Single_SaveNewScore("wagw", 1322);
+        fileManager.Single_SaveNewScore("wqwe", 1302);
+        fileManager.Single_SaveNewScore("aszxczxc", 52);
+
+        LoadSingleScores();
+    }
+
+
+    // Load Single Scores to UI
+    public void LoadSingleScores()
+    {
+        GameObject newTextObj = null;
+        //Text textCom = null;
+        TextMeshProUGUI textCom = null;
+        List<ScoreSaveData> data = fileManager.Single_LoadScores();
+
+        foreach (ScoreSaveData item in data)
+        {
+            // Instantiate new text and parent under Name Panel
+            newTextObj = Instantiate(text);
+            newTextObj.transform.parent = namePanel.transform;
+            textCom = newTextObj.GetComponent<TextMeshProUGUI>();
+            // Add the name
+            textCom.text += item.plyrName;
+
+            // Instantiate new text and parent under Score Panel
+            newTextObj = Instantiate(text);
+            newTextObj.transform.parent = scorePanel.transform;
+            textCom = newTextObj.GetComponent<TextMeshProUGUI>();
+            // Add the name
+            //textCom.text += item.plyrScore.ToString();
+        }
+
+    }
+}
