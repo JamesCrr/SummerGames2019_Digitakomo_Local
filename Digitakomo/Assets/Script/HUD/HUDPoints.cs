f2ebd97f9f56d443e05de9f0d7679ca820e8e439
Wei <g0546699m@gmail.com>
2019-07-16 15:03:56 +0300

    Added Score into HUD

---
+++ Digitakomo/Assets/Script/HUD/HUDPoints.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDPoints : MonoBehaviour
+{
+    Text pointsText;
+    int currentScore = 0;
+
+    // Start is called before first frame update
+    private void Start()
+    {
+        pointsText = GetComponent<Text>();
+        currentScore = ScoreCalculator.Instance.GetScore();
+    }
+
+    private void FixedUpdate()
+    {
+        // Has the Score Increased?
+        if (currentScore != ScoreCalculator.Instance.GetScore())
+        {
+            currentScore++;
+            pointsText.text = currentScore.ToString();
+        }
+    }
+}
