209bbb02b8a054a6713c9fd874137a565697aaff
Wei <g0546699m@gmail.com>
2019-07-16 10:25:36 +0300

    Added Score Calculator Script to MainMenu Scene

---
+++ Digitakomo/Assets/Script/ScoreBoard/ScoreCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCalculator : MonoBehaviour
+{
+    public static ScoreCalculator Instance = null;
+
+
+
+
+
+    // Awake
+    private void Awake()
+    {
+        // Attach Instance
+        if(Instance != null)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+
+
+}
